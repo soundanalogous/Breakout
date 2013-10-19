14679d91899df07c9ebd06580fc747cb4b3e7122
Jeff Hoefs <jeff.hoefs@gmail.com>
2013-10-19 17:28:08 -0400

    replacing GeneratorEvent.UPDATE in Pin.js with string value to eliminate dependency on GeneratorEvent in Pin object

--- src/core/Pin.js
+++ src/core/Pin.js
@@ -388,7 +388,8 @@ BO.Pin = (function () {
         addGenerator: function (newGenerator) {
             this.removeGenerator();
             this._generator = newGenerator;
-            this._generator.addEventListener(BO.generators.GeneratorEvent.UPDATE, this._autoSetValueCallback);
+            // BO.generators.GeneratorEvent.UPDATE = "update"
+            this._generator.addEventListener("update", this._autoSetValueCallback);
         },
 
         /**
@@ -397,7 +398,8 @@ BO.Pin = (function () {
          */
         removeGenerator: function () {
             if (this._generator !== null) {
-                this._generator.removeEventListener(BO.generators.GeneratorEvent.UPDATE, this._autoSetValueCallback);
+                // BO.generators.GeneratorEvent.UPDATE = "update"
+                this._generator.removeEventListener("update", this._autoSetValueCallback);
             }
             this._generator = null;             
         },
